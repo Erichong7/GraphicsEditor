eeaed5f75004162514a17ddcdbe7b4df9e2a1ed1
hongjeong-u <eirchong7@gmail.com>
2023-04-27 15:17:28 +0900

    Refactoring

--- GraphicsEditor/src/shapes/GLine.java
+++ GraphicsEditor/src/shapes/GLine.java
@@ -8,29 +8,41 @@ import shapes.GAnchor.EAnchors;
 public class GLine extends GShape {
 
 	private int px, py;
+	private int ox1, ox2, oy1, oy2;
 
 	public GLine() {
+	}
+
+	public GShape clone() {
+		return new GLine();
 	}
 
 	public void movePoint(int x2, int y2) {
 		Line2D line2D = (Line2D) shape;
 		line2D.setLine(line2D.getX1(), line2D.getY1(), x2, y2);
+		ox2 = x2;
+		oy2 = y2;
 	}
 
-	@Override
 	public void setShape(int x1, int y1, int x2, int y2) {
 		shape = new Line2D.Double(x1, y1, x2, y2);
+		ox1 = x1;
+		oy1 = y1;
 	}
 
-	@Override
-	public GShape clone() {
-		return new GLine();
+	public void setPoint(int x, int y) {
+		px = x;
+		py = y;
 	}
 
-	@Override
 	public void moveShape(int x2, int y2) {
 		Line2D line2D = (Line2D) shape;
 		line2D.setLine(line2D.getX1() + x2, line2D.getY1() + y2, line2D.getX2() + x2, line2D.getY2() + y2);
+	}
+
+	public void resizeShape(EAnchors selectedAnchor, int x, int y) {
+		// TODO Auto-generated method stub
+
 	}
 
 	public boolean onShape(Point p) {
@@ -39,18 +51,6 @@ public class GLine extends GShape {
 			return true;
 		}
 		return false;
-	}
-
-	@Override
-	public void resizeShape(EAnchors selectedAnchor, int x, int y) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void setPoint(int x, int y) {
-		px = x;
-		py = y;
 	}
 
 }
--- GraphicsEditor/src/shapes/GOval.java
+++ GraphicsEditor/src/shapes/GOval.java
@@ -12,6 +12,10 @@ public class GOval extends GShape {
 	public GOval() {
 	}
 
+	public GShape clone() {
+		return new GOval();
+	}
+
 	public void movePoint(int x2, int y2) {
 		Ellipse2D ellipse = (Ellipse2D) shape;
 		if (x2 <= ox && y2 <= oy) {
@@ -25,7 +29,6 @@ public class GOval extends GShape {
 		}
 	}
 
-	@Override
 	public void setShape(int x1, int y1, int x2, int y2) {
 		shape = new Ellipse2D.Double(x1, y1, x2 - x1, y2 - y1);
 		ox = x1;
@@ -37,12 +40,6 @@ public class GOval extends GShape {
 		py = y;
 	}
 
-	@Override
-	public GShape clone() {
-		return new GOval();
-	}
-
-	@Override
 	public void moveShape(int x, int y) {
 		Ellipse2D ellipse2D = (Ellipse2D) shape;
 		ellipse2D.setFrame(ellipse2D.getX() + x - px, ellipse2D.getY() + y - py, ellipse2D.getWidth(),
@@ -51,7 +48,6 @@ public class GOval extends GShape {
 		py = y;
 	}
 
-	@Override
 	public void resizeShape(EAnchors selectedAnchor, int x, int y) {
 		Ellipse2D ellipse = (Ellipse2D) shape;
 		int dx = x - px;
